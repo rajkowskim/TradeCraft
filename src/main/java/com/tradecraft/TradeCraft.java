a0e322462ec2581cdd3f5c4ca877216e686398b9
Rajkowski <Michal.Rajkowski@vimn.com>
2017-07-19 13:27:29 +0200

    Added more detailed summary.

--- src/main/java/com/tradecraft/TradeCraft.java
+++ src/main/java/com/tradecraft/TradeCraft.java
@@ -6,12 +6,14 @@ import com.tradecraft.data.DataLoader;
 import com.tradecraft.engine.Engine;
 import org.joda.time.DateTime;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class TradeCraft {
     public static void main(String[] args) {
-        int profit = 0;
+        List<Integer> profits = new ArrayList<Integer>();
 
         // sample sampleSize, stopLoss, stopWin, enterChance, cutoffHour
         int sampleSize = Integer.parseInt(args[0]);
@@ -24,17 +26,39 @@ public class TradeCraft {
         Map<DateTime, List<DataPoint>> map = DataLoader.loadData("dax.csv");
 
         for (int i = 0; i < sampleSize; i++) {
+            int oneLoopProfit = 0;
             for (DateTime x : map.keySet()) {
                 List<DataPoint> dataPoints = map.get(x);
                 Contract contract = engine.tryToEnter(dataPoints);
 
                 if (contract != null) {
-                    profit = profit + contract.getProfit();
+                    oneLoopProfit += contract.getProfit();
 //                    System.out.println(contract);
                 }
             }
+
+            profits.add(oneLoopProfit);
         }
 
-        System.out.println("Profit [pips]: " + profit / sampleSize);
+        displaySummary(profits);
+    }
+
+    private static double getAvg(List<Integer> profits) {
+        return profits.stream().mapToDouble(a -> a).average().orElse(0.0);
+    }
+
+    private static double variance(List<Integer> profits) {
+        double sum = profits.stream().mapToDouble(a -> a).sum();
+        double sumMinusAverage = sum - getAvg(profits);
+
+        return sumMinusAverage * sumMinusAverage / (profits.size()-1);
+    }
+
+    private static void displaySummary(List<Integer> profits) {
+        System.out.println("=====Summary=====");
+        System.out.println("Min: \t"+ Collections.min(profits));
+        System.out.println("Max: \t"+ Collections.max(profits));
+        System.out.println("Avg: \t"+ (int)getAvg(profits));
+        //System.out.println("Var: \t"+ (int)variance(profits));
     }
 }
